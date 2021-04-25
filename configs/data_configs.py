688d98b267aee11e9059c3309fd18ccd524d6c39
bayutop <82275180+bayutop@users.noreply.github.com>
2021-04-25 16:24:37 +0700

    Update data_configs.py

--- configs/data_configs.py
+++ configs/data_configs.py
@@ -3,19 +3,26 @@ from configs.paths_config import dataset_paths
 
 
 DATASETS = {
-	'ffhq_encode': {
+	'ffhq_encode1': {
 		'transforms': transforms_config.EncodeTransforms,
 		'train_source_root': dataset_paths['ffhq'],
 		'train_target_root': dataset_paths['ffhq'],
 		'test_source_root': dataset_paths['celeba_test'],
 		'test_target_root': dataset_paths['celeba_test']
 	},
-	'metfaces': {
+	'ffhq_encode2': {
 		'transforms': transforms_config.EncodeTransforms,
-		'train_source_root': dataset_paths['metfaces_train'],
-		'train_target_root': dataset_paths['metfaces_train'],
-		'test_source_root': dataset_paths['metfaces_test'],
-		'test_target_root': dataset_paths['metfaces_test']
+		'train_source_root': dataset_paths['ffhq'],
+		'train_target_root': dataset_paths['ffhq'],
+		'test_source_root': dataset_paths['celeba_test'],
+		'test_target_root': dataset_paths['celeba_test'],
+	},
+	'ffhq_frontalize': {
+		'transforms': transforms_config.FrontalizationTransforms,
+		'train_source_root': dataset_paths['ffhq'],
+		'train_target_root': dataset_paths['ffhq'],
+		'test_source_root': dataset_paths['celeba_test'],
+		'test_target_root': dataset_paths['celeba_test'],
 	},
 	"cars_encode": {
 		'transforms': transforms_config.CarsEncodeTransforms,
@@ -24,28 +31,14 @@ DATASETS = {
 		'test_source_root': dataset_paths['cars_test'],
 		'test_target_root': dataset_paths['cars_test']
 	},
-	"church_encode": {
+	"toonify1": {
 		'transforms': transforms_config.EncodeTransforms,
-		'train_source_root': dataset_paths['church_train'],
-		'train_target_root': dataset_paths['church_train'],
-		'test_source_root': dataset_paths['church_test'],
-		'test_target_root': dataset_paths['church_test']
+		'train_source_root': dataset_paths['ffhq'],
+		'train_target_root': dataset_paths['ffhq'],
+		'test_source_root': dataset_paths['celeba_test'],
+		'test_target_root': dataset_paths['celeba_test']
 	},
-	"horse_encode": {
-		'transforms': transforms_config.EncodeTransforms,
-		'train_source_root': dataset_paths['horse_train'],
-		'train_target_root': dataset_paths['horse_train'],
-		'test_source_root': dataset_paths['horse_test'],
-		'test_target_root': dataset_paths['horse_test']
-	},
-	"afhq_wild_encode": {
-		'transforms': transforms_config.EncodeTransforms,
-		'train_source_root': dataset_paths['afhq_wild_train'],
-		'train_target_root': dataset_paths['afhq_wild_train'],
-		'test_source_root': dataset_paths['afhq_wild_test'],
-		'test_target_root': dataset_paths['afhq_wild_test']
-	},
-	"toonify": {
+	"toonify2": {
 		'transforms': transforms_config.EncodeTransforms,
 		'train_source_root': dataset_paths['ffhq'],
 		'train_target_root': dataset_paths['ffhq'],
